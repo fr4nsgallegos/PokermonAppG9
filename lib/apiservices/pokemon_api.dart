0baa9d27c22c002b8edc85a98703eeaa4c946350
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-09-14 12:48:46 -0500

    retrofit gereator

---
+++ lib/apiservices/pokemon_api.dart
@@ -0,0 +1,20 @@
+// class PokemonApi {
+//   static const String pokemones =
+//       "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json?fbclid=IwZXh0bgNhZW0CMTAAAR1NvRhYdIz7HvFIbjRRZryG-5j4516YvSxaCzm4aH6RynNyp9tLZLTGz9Q_aem_OC7UyN2S8ri_YUImzNS2_g";
+// }
+
+import 'package:dio/dio.dart';
+import 'package:pokemonapp/models/pokemon_model_list.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'pokemon_api.g.dart';
+
+@RestApi(
+    baseUrl:
+        "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/")
+abstract class RestClient {
+  factory RestClient(Dio dio) = _RestClient;
+
+  @GET("pokedex.json")
+  Future<PokemonModelList> getPokemonList();
+}
