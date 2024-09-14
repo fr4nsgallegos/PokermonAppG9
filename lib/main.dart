4ff59acff98114c615dcc7227b6f7d695ecde909
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-09-14 10:03:39 -0500

    init

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:pokemonapp/home_page.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
